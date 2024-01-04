be0368d604f20ccc5812118ea617edcb02d663dd
Lucas <super.ghast8@gmail.com>
2024-01-04 09:45:37 -0300

    first commit

---
+++ Anatomia_de_classes/anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,18 @@
+public class MinhaClasse {
+    
+
+public static void main(String[] args) {
+    
+System.out.println("Ola turma, sejam bem-vindos");
+
+int ano = 2024;
+
+ano = 2022;
+
+final String BR = "Brasil";
+
+BR = "Brazil";
+
+}
+
+}
