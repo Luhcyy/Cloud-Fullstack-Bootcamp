96aa427553a9c9efaa501a49953406175b9a1b7e
Lucas <super.ghast8@gmail.com>
2024-01-05 23:31:18 -0300

    Adicionar aula de Operadores

---
+++ Java/Operadores/Operadores/src/Operadores.java
@@ -0,0 +1,96 @@
+public class Operadores {
+    public static void main(String[] args) {
+       
+      boolean condicao1 = true;
+      boolean condicao2 = true;
+      
+      if(condicao1 && (7 > 4)){
+        System.out.println("As duas condições são verdadeiras");
+      }
+      
+       if(condicao1 || condicao2){
+        System.out.println("Uma das condições é verdadeira");
+      }
+
+      System.out.println("fim");
+
+
+    /* 
+        int numero1 = 1;
+        int numero2 = 2;
+
+
+        boolean simNao = numero1 == numero2;
+
+
+        System.out.println("numeroUm é igual o numeroDois? " + simNao);
+
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente o numeroDois? " + simNao);
+
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior o numeroDois? " + simNao);
+
+    */
+
+
+
+
+    /* 
+        int a, b;
+
+
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso"; 
+
+
+        System.out.println(resultado);
+    */
+
+       /*
+        boolean variavel = true;
+
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+        */
+
+        /* 
+        int numero = 5;
+        //x repeticao
+        numero++;   
+        //numero = numero + 2;
+
+        System.out.println( ++ numero);
+
+        System.out.println( -- numero );
+
+        */
+
+
+
+
+
+
+       /* int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+        
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+        */
+    }
+}
